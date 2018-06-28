cab670e537199900e3133bbaddccfa1c1fcbd5b8
Sameh16 <sameh_mohamed16@hotmail.com>
2018-06-28 19:17:48 +0200

    rateEdit

--- src/main/java/com/example/RestController/TripRestController.java
+++ src/main/java/com/example/RestController/TripRestController.java
@@ -63,23 +63,11 @@ public class TripRestController {
 	//Modified by Mariam 
 	//Modified By Sameh
 	
-	
 	@RequestMapping(value = "/saveTrip/{truck_id}/{driver_id}/{parent_id}/{road_id}/{date}/{good}", method = RequestMethod.GET)
 	public Map<String, String> saveTrip(@PathVariable String truck_id, @PathVariable String date,
 			@PathVariable long driver_id, @PathVariable long parent_id, @PathVariable long road_id,@PathVariable String good) {
 		Map<String, String> res = new HashMap<>();
 		int flag=0;
-		if(truckRepository.findOne(truck_id)!=null)
-		{
-			Truck truck = truckRepository.findOne(truck_id);
-			Road road = roadRepository.findOne(road_id);
-			if (truck == null) {
-@RequestMapping(value = "/saveTrip/{truck_id}/{driver_id}/{parent_id}/{road_id}/{date}/{good}", method = RequestMethod.GET)
-	public Map<String, String> saveTrip(@PathVariable String truck_id, @PathVariable String date,
-			@PathVariable long driver_id, @PathVariable long parent_id, @PathVariable long road_id,@PathVariable String good) {
-		Map<String, String> res = new HashMap<>();
-		int flag=0;
-		String bar="";
 		if(truckRepository.findOne(truck_id)!=null)
 		{
 			Truck truck = truckRepository.findOne(truck_id);
@@ -112,90 +100,50 @@ public class TripRestController {
 							trip.setTruck(truck);
 							trip.setRoad(road);
 							trip.setState(1);
-							//check if these bar codes are existed	
-							String[] barcode=good.split(",");
-							for(int i=0;i<barcode.length;i++)
+							if (tripRepository.save(trip) != null) 
 							{
-								String[] CountsWithGoods=barcode[i].split(":");
-								Good g=goodRepository.findOne(CountsWithGoods[0]);
-								if(g==null)
-								{
-									flag=3;
-									bar=CountsWithGoods[0];
-									break;
-								}
-							}
-							if(flag==3)
-							{
-					    		res.put("Error", "There is no good with this barcode: "+bar);
-							}
-							else
-							{
-
-								//check of availability 
+								String[] barcode=good.split(",");
 								for(int i=0;i<barcode.length;i++)
 								{
 									String[] CountsWithGoods=barcode[i].split(":");
-									Good g=goodRepository.findOne(CountsWithGoods[0]);
-							    	ArrayList<TripGood> tripGoods = (ArrayList<TripGood>)tripGoodRepository.findAllByGood(g);
-									int counter=0;
-									for(int j=0;j<tripGoods.size();j++)
+									if(goodRepository.findOne(CountsWithGoods[0])!=null)
 									{
-										counter+=tripGoods.get(j).getNum_of_goods();
+										TripGood tripGood=new TripGood();
+										tripGood.setTrip(trip);
+								    	Good g=goodRepository.findOne(CountsWithGoods[0]);
+								    	tripGood.setGood(g);
+								    	tripGood.setNum_of_goods(Integer.parseInt(CountsWithGoods[1]));
+								    	tripGood.setScan_in_num_of_goods(0);
+								    	tripGood.setScan_out_num_of_goods(0);
+								    	tripGood.setState(0);
+								    	if (tripGoodRepository.save(tripGood) == null) {
+								    		flag=1;
+								    		break;
+								    	}
+									 }
+									else
+									{
+										flag=2;
+										break;
 									}
-							    	int num=g.getNum_of_goods();
-							    	System.out.println(num);
-							    	System.out.println(counter);
-							    	int avail=num-counter;
-							    	System.out.println(avail);
-							    	int newNum=Integer.parseInt(CountsWithGoods[1]);
-							    	System.out.println(newNum);
-							    	if(newNum>avail)
-							    	{
-							    		flag=3;
-							    		bar=g.getBarcode();
-							    		break;
-							    	}
 								}
-								if(flag==3)
+								if(flag==1)
 								{
-						    		res.put("Error", "There is not available goods for this barcode: "+bar);
+						    		res.put("Error", "Connection Error");
+								}
+								else if(flag==0)
+								{
+						    		res.put("Success", "Trip is added");
 								}
 								else
 								{
-									if (tripRepository.save(trip) != null) 
-									{
-										for(int i=0;i<barcode.length;i++)
-										{
-											String[] CountsWithGoods=barcode[i].split(":");
-											TripGood tripGood=new TripGood();
-											tripGood.setTrip(trip);
-									    	Good g=goodRepository.findOne(CountsWithGoods[0]);
-									    	tripGood.setGood(g);
-								    		tripGood.setNum_of_goods(Integer.parseInt(CountsWithGoods[1]));
-									    	tripGood.setScan_in_num_of_goods(0);
-									    	tripGood.setScan_out_num_of_goods(0);
-									    	tripGood.setState(0);
-									    	if (tripGoodRepository.save(tripGood) == null) {
-									    		flag=1;
-									    		break;
-									    	}
-										   
-										}
-										if(flag==1)
-										{
-								    		res.put("Error", "Connection Error by saving Trip Goods");
-										}
-										else if(flag==0)
-										{
-								    		res.put("Success", "Trip is added");
-										}
-									}
-									else 
-									{
-										res.put("Error", "Connection Error by saving Trip");
-									}
-								}	
+						    		res.put("Error", "There is no good with this barcode");
+								}
+								System.out.println(barcode.length);    
+							}
+							else 
+							{
+								res.put("Error", "Connection Error");
 							}
 						}
 						else
