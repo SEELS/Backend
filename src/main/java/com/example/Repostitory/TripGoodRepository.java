14b78ab93bb4f308f2925755a507cf5d786a7ad7
Sameh16 <sameh_mohamed16@hotmail.com>
2018-07-02 23:55:11 +0200

    Yes wla eh

--- src/main/java/com/example/Repostitory/TripGoodRepository.java
+++ src/main/java/com/example/Repostitory/TripGoodRepository.java
@@ -14,4 +14,5 @@ public interface TripGoodRepository extends CrudRepository<TripGood, Long>
 	ArrayList<TripGood>  findAllByTrip(Trip trip);
 	ArrayList<TripGood> findAllByGood(Good good);
 	public TripGood findByTripAndGood(Trip trip,Good good);
+	public TripGood findFirstByGoodOrderByDesc(Good good);
 }
